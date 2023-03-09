3718fbeedad29b286ac1ed93e320ba0ed068e075
Perla Polito <perla.polito@zextras.com>
2023-03-09 12:50:36 +0100

    chore: [UM-11] apply requested changes

--- src/main/java/com/zextras/carbonio/usermanagement/UserManagementClient.java
+++ src/main/java/com/zextras/carbonio/usermanagement/UserManagementClient.java
@@ -4,6 +4,7 @@
 
 package com.zextras.carbonio.usermanagement;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zextras.carbonio.usermanagement.entities.UserId;
 import com.zextras.carbonio.usermanagement.entities.UserInfo;
@@ -13,7 +14,9 @@ import com.zextras.carbonio.usermanagement.exceptions.UserNotFound;
 import io.vavr.control.Try;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -28,6 +31,7 @@ import org.apache.http.impl.client.HttpClients;
 public class UserManagementClient {
 
   private final String validateTokenEndpoint   = "/auth/token/";
+  private final String getUsersEndpoint        = "/users/";
   private final String getUsersByIdEndpoint    = "/users/id/";
   private final String getUsersByEmailEndpoint = "/users/email/";
   private final String healthEndpoint          = "/health/";
@@ -101,6 +105,49 @@ public class UserManagementClient {
       }
 
       return Try.failure(new UnAuthorized());
+    } catch (IOException exception) {
+      return Try.failure(new InternalServerError(exception));
+    }
+  }
+
+  /**
+   * Allows to retrieve a list of Carbonio users by {@link UUID} if they exist.
+   *
+   * @param cookie    is a {@link String} representing the requester cookie. (It is necessary to
+   *                  have only the ZM_AUTH_TOKEN cookie).
+   * @param usersUUID is a {@link List} of {@link UUID}s of the Carbonio users to retrieve.
+   * @return a {@link Try#success} containing a {@link List} of {@link UserInfo} representing
+   * the related Carbonio users if they exist. <p />It returns a {@link Try#failure} containing
+   * an {@link UserNotFound} throwable the cookie is not valid. <p />It returns a
+   * {@link Try#failure} containing an {@link InternalServerError} throwable if something goes
+   * wrong during the API call.
+   */
+  public Try<List<UserInfo>> getUsers(
+    String cookie,
+    List<UUID> usersUUID
+  ) {
+    CloseableHttpClient httpClient = HttpClients.createMinimal();
+
+    AtomicReference<String> getRequest = new AtomicReference<>(userManagementURL + getUsersEndpoint + "?userIds=");
+    usersUUID.forEach(userUUID -> getRequest.updateAndGet(v -> v + userUUID));
+    HttpGet request = new HttpGet(getRequest.get());
+    request.setHeader("Cookie", cookie);
+
+    try {
+      CloseableHttpResponse response = httpClient.execute(request);
+
+      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+        String bodyResponse = IOUtils.toString(
+          response.getEntity().getContent(),
+          StandardCharsets.UTF_8
+        );
+
+        return Try.success(new ObjectMapper().readValue(bodyResponse, new TypeReference<>() {}));
+      }
+
+      return Try.failure(
+        new UserNotFound("The cookie: \"" + cookie + "\" is not valid")
+      );
     } catch (IOException exception) {
       return Try.failure(new InternalServerError(exception));
     }
