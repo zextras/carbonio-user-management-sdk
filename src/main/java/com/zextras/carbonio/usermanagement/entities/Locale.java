1fee8d1a0e4fa2dcb6ed4ecb92331b0a13904bfd
Federico Rispo <federico.rispo@zextras.com>
2023-08-31 10:51:30 +0200

    feat: expose the /users/myself/ API in the UserManagementClient (#5)
    - Added the UserMyself POJO
    - Added the Locale enum containing all the supported localed in Carbonio
    - Implemented the getUserMyself method to call the /users/myself API
    
    refs: UM-20

---
+++ src/main/java/com/zextras/carbonio/usermanagement/entities/Locale.java
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package com.zextras.carbonio.usermanagement.entities;
+
+/**
+ * Represents all the locales that a user can choose in Carbonio.
+ */
+public enum Locale {
+  DE,
+  EN,
+  ES,
+  FR,
+  HI,
+  IT,
+  JA,
+  NL,
+  PL,
+  PT,
+  PT_BR,
+  RO,
+  RU,
+  TH,
+  TR,
+  VI,
+  ZH_CN
+}
---
+++ src/main/java/com/zextras/carbonio/usermanagement/entities/UserMyself.java
@@ -0,0 +1,69 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package com.zextras.carbonio.usermanagement.entities;
+
+/**
+ * Contains all the information (for now only a subset of them) of a Carbonio user.
+ */
+public class UserMyself {
+
+  private UserId id;
+  private String email;
+  private String fullName;
+  private String domain;
+  private Locale locale;
+
+  public UserMyself() {}
+
+  public UserMyself(
+    UserId id,
+    String email,
+    String fullName,
+    String domain,
+    Locale locale
+  ) {
+    this.id = id;
+    this.email = email;
+    this.fullName = fullName;
+    this.domain = domain;
+    this.locale = locale;
+  }
+
+  public UserId getId() {
+    return id;
+  }
+
+  public void setId(UserId id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public String getDomain() {
+    return domain;
+  }
+
+  public void setDomain(String domain) {
+    this.domain = domain;
+  }
+
+  public Locale getLocale() {
+    return locale;
+  }
+}
