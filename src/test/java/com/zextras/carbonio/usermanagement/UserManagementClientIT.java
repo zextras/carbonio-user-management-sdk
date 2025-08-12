f43bba09e3110c261eff07e49ee246a166ef2ccd
Matteo Baglini <matteo.baglini@gmail.com>
2025-08-12 11:37:14 +0200

    chore: clean up

--- src/test/java/com/zextras/carbonio/usermanagement/UserManagementClientIT.java
+++ src/test/java/com/zextras/carbonio/usermanagement/UserManagementClientIT.java
@@ -8,52 +8,17 @@ import com.zextras.carbonio.usermanagement.support.DockerComposeExtension;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testcontainers.containers.ComposeContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-//@Testcontainers
 @ExtendWith(DockerComposeExtension.class)
 public class UserManagementClientIT {
-
-//  @Container
-//  public ComposeContainer environment = new ComposeContainer(new File("docker-compose.yaml"))
-////          .withLocalCompose(true)
-//          ;
-
-//  // We'll map container's 8080 to host 20000 so tests can target http://127.78.0.1:20000
-//  @Container
-//  @SuppressWarnings("resource")
-//  private static final GenericContainer<?> WIREMOCK = new GenericContainer<>(
-//      DockerImageName.parse("wiremock/wiremock:2.35.0")
-//  )
-//      .withExposedPorts(8080)
-//      .withCommand("--port", "8080")
-//      .withCreateContainerCmdModifier(cmd -> {
-//          com.github.dockerjava.api.model.Ports portBindings = new com.github.dockerjava.api.model.Ports();
-//          portBindings.bind(
-//              com.github.dockerjava.api.model.ExposedPort.tcp(8080),
-//              com.github.dockerjava.api.model.Ports.Binding.bindPort(20000)
-//          );
-//          com.github.dockerjava.api.model.HostConfig hostConfig = cmd.getHostConfig();
-//          if (hostConfig == null) {
-//            hostConfig = new com.github.dockerjava.api.model.HostConfig();
-//          }
-//          hostConfig.withPortBindings(portBindings);
-//          cmd.withHostConfig(hostConfig);
-//      })
-//          ;
 
   private static final String BASE_URL = "http://127.0.0.1:20001";
   private static UserManagementClient client;
 
   @BeforeAll
   static void setUpAll() {
-//    assertTrue(WIREMOCK.isRunning(), "WireMock container should be running");
     client = UserManagementClient.atURL(BASE_URL);
   }
 
