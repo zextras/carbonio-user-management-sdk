f70dbcdbed47e81c822cf059b215eccdf445790a
Matteo Baglini <matteo.baglini@gmail.com>
2025-08-12 09:40:34 +0200

    test: make IT test run with the real server

--- src/test/java/com/zextras/carbonio/usermanagement/UserManagementClientIT.java
+++ src/test/java/com/zextras/carbonio/usermanagement/UserManagementClientIT.java
@@ -4,61 +4,58 @@
 
 package com.zextras.carbonio.usermanagement;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.ComposeContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Testcontainers
+//@Testcontainers
 public class UserManagementClientIT {
 
-  // We'll map container's 8080 to host 20000 so tests can target http://127.78.0.1:20000
-  @Container
-  @SuppressWarnings("resource")
-  private static final GenericContainer<?> WIREMOCK = new GenericContainer<>(
-      DockerImageName.parse("wiremock/wiremock:2.35.0")
-  )
-      .withExposedPorts(8080)
-      .withCommand("--port", "8080")
-      .withCreateContainerCmdModifier(cmd -> {
-          com.github.dockerjava.api.model.Ports portBindings = new com.github.dockerjava.api.model.Ports();
-          portBindings.bind(
-              com.github.dockerjava.api.model.ExposedPort.tcp(8080),
-              com.github.dockerjava.api.model.Ports.Binding.bindPort(20000)
-          );
-          com.github.dockerjava.api.model.HostConfig hostConfig = cmd.getHostConfig();
-          if (hostConfig == null) {
-            hostConfig = new com.github.dockerjava.api.model.HostConfig();
-          }
-          hostConfig.withPortBindings(portBindings);
-          cmd.withHostConfig(hostConfig);
-      })
-          ;
+//  @Container
+//  public ComposeContainer environment = new ComposeContainer(new File("docker-compose.yaml"))
+////          .withLocalCompose(true)
+//          ;
 
-  private static final String BASE_URL = "http://127.78.0.1:20000";
+//  // We'll map container's 8080 to host 20000 so tests can target http://127.78.0.1:20000
+//  @Container
+//  @SuppressWarnings("resource")
+//  private static final GenericContainer<?> WIREMOCK = new GenericContainer<>(
+//      DockerImageName.parse("wiremock/wiremock:2.35.0")
+//  )
+//      .withExposedPorts(8080)
+//      .withCommand("--port", "8080")
+//      .withCreateContainerCmdModifier(cmd -> {
+//          com.github.dockerjava.api.model.Ports portBindings = new com.github.dockerjava.api.model.Ports();
+//          portBindings.bind(
+//              com.github.dockerjava.api.model.ExposedPort.tcp(8080),
+//              com.github.dockerjava.api.model.Ports.Binding.bindPort(20000)
+//          );
+//          com.github.dockerjava.api.model.HostConfig hostConfig = cmd.getHostConfig();
+//          if (hostConfig == null) {
+//            hostConfig = new com.github.dockerjava.api.model.HostConfig();
+//          }
+//          hostConfig.withPortBindings(portBindings);
+//          cmd.withHostConfig(hostConfig);
+//      })
+//          ;
+
+  private static final String BASE_URL = "http://127.0.0.1:20001";
   private static UserManagementClient client;
 
   @BeforeAll
   static void setUpAll() {
-    assertTrue(WIREMOCK.isRunning(), "WireMock container should be running");
-    WireMock.configureFor("127.78.0.1", 20000);
+//    assertTrue(WIREMOCK.isRunning(), "WireMock container should be running");
     client = UserManagementClient.atURL(BASE_URL);
   }
 
   @Test
   void healthCheck_true() {
-    stubFor(get(urlEqualTo("/health/"))
-            .willReturn(aResponse().withStatus(200)));
-
     assertTrue(client.healthCheck());
   }
 }
